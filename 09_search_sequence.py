8fbc07b410d3e00b4d1330d7024b777c7c86feba
jesusjzp <jesusjzp@gmail.com>
2013-09-24 13:24:31 +0200

    new post

--- 09_search_sequence.py
+++ 09_search_sequence.py
@@ -12,20 +12,23 @@ def find(arr):
         beg = end = i
         if i in table:
             continue
+        
         table[i] = 'EXISTED'
         if i - 1 in table:
             beg = table[i-1]
         if i + 1 in table:
             end = table[i+1]
+        
         table[beg] = end
         table[end] = beg
+        
         if end - beg > last - first:
             first = beg
             last = end
-        # print 'i: ', i
-        # print 'table: ', table
-        # print 'first: ', first, ' last: ', last
-        # print '------'
+        print 'i: ', i
+        print 'table: ', table
+        print 'first: ', first, ' last: ', last
+        print '------'
     return list(range(first, last + 1))
 
 arr = [3, 6, 10, 4, 7, 9, 1, 8, 11]
